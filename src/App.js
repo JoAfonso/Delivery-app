73b665bb4f27a82819b0a2f97f6ac7d82ffd4c71
JoAfonso <joao-a-@hotmail.com>
2023-03-08 10:31:31 -0300

    continuando

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meal from "./components/Meals/Meal";
 
 function App() {
   return (
     <div>
       <Header />
+      <main>
+        <Meal/>
+      </main>
     </div>
   );
 }
---
+++ src/components/Meals/Meal.js
@@ -0,0 +1,14 @@
+
+import AvaliableMeals from "./AvaliableMeals";
+import MealsSummary from "./MealSummary";
+
+const Meal = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvaliableMeals />
+    </>
+  );
+};
+
+export default Meal;
