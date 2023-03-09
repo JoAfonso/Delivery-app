4db13c45becda93fcc3cfb064912b53fb046df14
JoAfonso <joao-a-@hotmail.com>
2023-03-09 17:17:22 -0300

    arrumando o Contexto

--- src/components/store/CartProvider.js
+++ src/components/store/CartProvider.js
@@ -1,10 +1,19 @@
-import CartContex from "./cart-contex";
+import React from "react";
+// import CartContex from "./cart-contex";
 
 const CartProvider = (props) => {
+
+  const CartContext = React.createContext({
+    items: [],
+    totalAmount: 0,
+    addItem: (item) => {},
+    removeItem: (id) => {}
+  });
+
   const addItemCartHandler = (item) => {};
   const removeItemCartHandler = (id) => {};
 
-  const cartContex = {
+  const cartContext = {
     items: [],
     totalAmount: 0,
     addItem: addItemCartHandler,
@@ -12,9 +21,9 @@ const CartProvider = (props) => {
   };
 
   return (
-    <CartContex.Provider value={cartContex}>
-      {props.chilcren}
-    </CartContex.Provider>
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
   );
 };
 
